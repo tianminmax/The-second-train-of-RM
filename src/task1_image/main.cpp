f582996ee83ea87915b3bb72ceebd36388319c19
tianmin.liu <tianminmax@gmail.com>
2026-09-25 14:23:27 +0800

    任务一提交

--- src/task1_image/main.cpp
+++ src/task1_image/main.cpp
@@ -17,7 +17,7 @@ int main() {
     // 灰度图
     cv::Mat gray;
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-    cv::imwrite("gray.png", gray);
+    cv::imwrite("result/task1_images/gray.png", gray);
 
     // HSV 图
     cv::Mat hsv;
@@ -26,9 +26,9 @@ int main() {
     // 拆分 H、S、V
     std::vector<cv::Mat> hsv_channels;
     cv::split(hsv, hsv_channels);
-    cv::imwrite("hsv_h.png", hsv_channels[0]);
-    cv::imwrite("hsv_s.png", hsv_channels[1]);
-    cv::imwrite("hsv_v.png", hsv_channels[2]);
+    cv::imwrite("result/task1_images/hsv_h.png", hsv_channels[0]);
+    cv::imwrite("result/task1_images/hsv_s.png", hsv_channels[1]);
+    cv::imwrite("result/task1_images/hsv_v.png", hsv_channels[2]);
 
     std::cout << "第1步完成: gray.png / hsv_h.png / hsv_s.png / hsv_v.png" << std::endl;
 
@@ -39,15 +39,15 @@ int main() {
 
     // 均值滤波 5x5
     cv::blur(img, meanImg, cv::Size(5, 5));
-    cv::imwrite("mean_filter.png", meanImg);
+    cv::imwrite("result/task1_images/mean_filter.png", meanImg);
 
     // 高斯滤波 5x5, sigmaX=1.5
     cv::GaussianBlur(img, gaussianImg, cv::Size(5, 5), 1.5);
-    cv::imwrite("gaussian_filter.png", gaussianImg);
+    cv::imwrite("result/task1_images/gaussian_filter.png", gaussianImg);
 
     // 中值滤波 核=5
     cv::medianBlur(img, medianImg, 5);
-    cv::imwrite("median_filter.png", medianImg);
+    cv::imwrite("result/task1_images/median_filter.png", medianImg);
 
     std::cout << "第2步完成: mean_filter.png / gaussian_filter.png / median_filter.png" << std::endl;
 
@@ -59,7 +59,7 @@ int main() {
     cv::inRange(hsv, cv::Scalar(0, 100, 100), cv::Scalar(10, 255, 255), mask_low);
     cv::inRange(hsv, cv::Scalar(170, 100, 100), cv::Scalar(179, 255, 255), mask_high);
     red_mask = mask_low | mask_high;
-    cv::imwrite("red_mask.png", red_mask);
+    cv::imwrite("result/task1_images/red_mask.png", red_mask);
 
     std::cout << "第3步完成: red_mask.png" << std::endl;
 
@@ -74,10 +74,10 @@ int main() {
     cv::morphologyEx(red_mask, opened, cv::MORPH_OPEN, kernel);
     cv::morphologyEx(red_mask, closed, cv::MORPH_CLOSE, kernel);
 
-    cv::imwrite("erode.png", eroded);
-    cv::imwrite("dilate.png", dilated);
-    cv::imwrite("open.png", opened);
-    cv::imwrite("close.png", closed);
+    cv::imwrite("result/task1_images/erode.png", eroded);
+    cv::imwrite("result/task1_images/dilate.png", dilated);
+    cv::imwrite("result/task1_images/open.png", opened);
+    cv::imwrite("result/task1_images/close.png", closed);
 
     // 用开运算后的结果找轮廓
     std::vector<std::vector<cv::Point>> contours;
@@ -99,7 +99,7 @@ int main() {
         std::cout << "轮廓 " << i << " 面积=" << area
                   << " 外接矩形=" << box << std::endl;
     }
-    cv::imwrite("contours_boxes.png", contour_result);
+    cv::imwrite("result/task1_images/contours_boxes.png", contour_result);
 
     std::cout << "第4步完成: erode/dilate/open/close/contours_boxes.png" << std::endl;
 
@@ -112,20 +112,20 @@ int main() {
     cv::rectangle(draw_img, cv::Rect(300, 100, 150, 100), cv::Scalar(255, 0, 0), 3);
     cv::putText(draw_img, "OpenCV Demo", cv::Point(50, 50),
                 cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 0, 255), 2);
-    cv::imwrite("drawing.png", draw_img);
+    cv::imwrite("result/task1_images/drawing.png", draw_img);
 
     // 5.2 绕图像中心旋转 35 度
     cv::Point2f center(img.cols / 2.0f, img.rows / 2.0f);
     cv::Mat rot = cv::getRotationMatrix2D(center, 35.0, 1.0);
     cv::Mat rotated;
     cv::warpAffine(img, rotated, rot, img.size());
-    cv::imwrite("rotated_35deg.png", rotated);
+    cv::imwrite("result/task1_images/rotated_35deg.png", rotated);
 
     // 5.3 裁剪左上角 1/4
     int w = img.cols / 2;
     int h = img.rows / 2;
     cv::Mat crop = img(cv::Rect(0, 0, w, h));
-    cv::imwrite("crop_top_left.png", crop);
+    cv::imwrite("result/task1_images/crop_top_left.png", crop);
 
     std::cout << "第5步完成: drawing.png / rotated_35deg.png / crop_top_left.png" << std::endl;
 
